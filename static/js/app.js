d0d62d983ccb866722ff65e698569b3201e2b056
Joost Baaij <joost@spacebabies.nl>
2017-10-09 00:13:40 +0200

    fix the reveal button
    it was the stupid clicky animation

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,8 @@
-window.addEventListener('load', function() {
+var revealButton = document.querySelector("button.reveal-solution");
+var solution = document.querySelector(".solution");
 
-  var revealButton = document.querySelector(".reveal-solution");
-  var solution = document.querySelector(".solution");
+if(revealButton && solution) {
   revealButton.addEventListener("click", function(e) {
-    e.preventDefault();
     solution.classList.toggle('invisible');
-  }, false);
-
-});
+  });
+}
